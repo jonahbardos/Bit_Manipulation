c57f64af72d5eb8f46de4ba9bb62780d1013a8ec
Jonah Bardos <jonahbardos@gmail.com>
2021-07-11 17:48:22 -0700

    Add getBit

--- bitoperators.c
+++ bitoperators.c
@@ -21,10 +21,21 @@
 
 */
 #include <stdio.h>
+#include <stdbool.h>
+bool getBit(int num, int i);
 int main(){
  int a = 0x04;
  a = a >> 2;
  printf("value of a: %X [%x]\n",a,a);
-
+ 
+ bool bitt = getBit(0xff, 7);
+ printf(bitt ? "True" : "False");
  return 0;
+}
+
+
+// Checks to see if a bit is set.
+// 0 == LSB and 7 == MSB
+bool getBit(int num, int i){
+ return ((num & (1 << i)) != 0);
 }
