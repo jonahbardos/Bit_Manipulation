a23844aa3db61db05f04277e2fd44867745fa65f
Jonah Bardos <jonahbardos@gmail.com>
2021-07-11 23:40:45 -0700

    Added function to check all bits that are set in a byte

--- bitoperators.c
+++ bitoperators.c
@@ -24,14 +24,18 @@
 #include <stdbool.h>
 #define SWAP(a,b) a^=b; b^=a; a^=b; // multiple statements require semi-colons
 
+// Function protypes tell the compiler what they return
 bool getBit(int num, int i);
+void checkAllBits(int num);
+
 int main(){
    int a = 0x04;
    a = a >> 2;
    printf("value of a: %X [%x]\n",a,a);
 
    bool bitt = getBit(0xff, 7);
-   printf(bitt ? "True" : "False");
+   printf(bitt ? "True\n" : "False\n");
+   checkAllBits(0xfe);
    return 0;
 }
 
@@ -40,4 +44,14 @@ int main(){
 // 0 == LSB and 7 == MSB
 bool getBit(int num, int i){
    return ((num & (1 << i)) != 0);
+}
+
+// Checks all the bits that are set in a byte
+void checkAllBits(int num) {
+   printf("Checking all the bits in a byte\n");
+   for(int i=0; i<8; i++){
+      if ((num & (1 << i)) != 0){
+         printf("Bit %d is set \n", i);
+      }
+   }
 }
